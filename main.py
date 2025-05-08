7303b6c7b8fd411195fbc4ff7078bd0eee8ecb24
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-05-08 23:15:37 +0630

    Todo Day 2 - 2 Getting User Input Repeatedly

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 user_prompt = "Enter your todo: "
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
 
-todos = [todo1, todo2, todo3]
-print(todos)
-print(type(todos))
+while True:
+    todo = input(user_prompt)
+    print(todo)
+    print("Next...")
+
