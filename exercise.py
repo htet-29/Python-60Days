c71c306bd24504262d0508befcbbfd1ac492c82e
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-05-10 22:47:08 +0630

    Todo Day 3  Exercise 1

---
+++ exercise.py
@@ -0,0 +1,11 @@
+country = input("What is your country? ")
+
+match country.upper():
+    case 'USA':
+        print("Hello")
+    case 'INDIA':
+        print("Namaste")
+    case "GERMANY":
+        print("Hallo")
+
+
