67c993bcdfa452eee198c992920b7deacae18832
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-05-11 23:38:02 +0630

    Todo Day 5 - 4 : Add a Complete Todo Feature

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Enter add, show, edit or exit: ")
+    user_action = input("Enter add, show, edit, complete or exit: ")
     user_action = user_action.lower().strip()
 
     match user_action:
@@ -10,13 +10,16 @@ while True:
             todos.append(todo.capitalize())
         case "show":
             for index, item in enumerate(todos):
-                row = f"{index}-{item}"
+                row = f"{index + 1}-{item}"
                 print(row)
         case "edit":
             number = int(input("Number of the todo to edit: "))
             number = number -1
             new_todo = input("Enter a new todo: ")
             todos[number] = new_todo.capitalize()
+        case "complete":
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number - 1)
         case "exit":
             break
 
