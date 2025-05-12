a009a69ca7e41794334bc00a483866471ebec31c
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-05-12 08:05:27 +0630

    Todo Day 5 - 5 : Bonus 1

--- bonus.py
+++ bonus.py
@@ -1,7 +1,29 @@
-file_names = ["1.Refactor.txt", "2.Animation.txt", "3.Videos.txt"]
+todos = []
 
-for file_name in file_names:
-    file_name = file_name.replace(".", "-", 1)
-    print(file_name)
+while True:
+    user_action = input("Enter add, show, edit, complete or exit: ")
+    user_action = user_action.lower().strip()
 
+    match user_action:
+        case "add":
+            todo = input("Enter a todo: ")
+            todos.append(todo.capitalize())
+        case "show":
+            for index, item in enumerate(todos):
+                row = f"{index + 1}-{item}"
+                print(row)
+            print(f"Length of todos : {index+1}. The last item is {item}")
+            print(f"Length of todos : {len(todos)}. The last item is {todos[-1]}")
+        case "edit":
+            number = int(input("Number of the todo to edit: "))
+            number = number -1
+            new_todo = input("Enter a new todo: ")
+            todos[number] = new_todo.capitalize()
+        case "complete":
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number - 1)
+        case "exit":
+            break
+
+print("Bye!")
 
