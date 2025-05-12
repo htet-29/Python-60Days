d1172831fddf6e12f1bd56d52fd84d3497824181
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-05-12 08:06:28 +0630

    Todo Day 5 - 5 : Bonus 2

--- bonus.py
+++ bonus.py
@@ -1,29 +1,8 @@
-todos = []
+b = enumerate("Hello")
 
-while True:
-    user_action = input("Enter add, show, edit, complete or exit: ")
-    user_action = user_action.lower().strip()
+b = list(b)
+print(b)
 
-    match user_action:
-        case "add":
-            todo = input("Enter a todo: ")
-            todos.append(todo.capitalize())
-        case "show":
-            for index, item in enumerate(todos):
-                row = f"{index + 1}-{item}"
-                print(row)
-            print(f"Length of todos : {index+1}. The last item is {item}")
-            print(f"Length of todos : {len(todos)}. The last item is {todos[-1]}")
-        case "edit":
-            number = int(input("Number of the todo to edit: "))
-            number = number -1
-            new_todo = input("Enter a new todo: ")
-            todos[number] = new_todo.capitalize()
-        case "complete":
-            number = int(input("Number of the todo to complete: "))
-            todos.pop(number - 1)
-        case "exit":
-            break
 
-print("Bye!")
-
+for i, j in [(0, 'H'), (1, 'e'), (2, 'l'), (3, 'l'), (4, 'o')]:
+    print(i, j)
