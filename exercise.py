b86cdd26e795442f8a17e3c60c657dfaacc0d028
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-05-14 08:45:55 +0630

    Todo Day 6  : Exercise 5

--- exercise.py
+++ exercise.py
@@ -1,6 +1,14 @@
-file = open('file.txt', 'w')
-file.write("snail")
+file = open('members.txt', 'r')
+members = file.readlines()
 file.close()
+
+name = input("Enter a new member: ") + '\n'
+
+file = open('members.txt', 'w')
+members.append(name)
+file.writelines(members)
+file.close()
+
 
 
 
