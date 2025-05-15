af7b67c4a059b65ed6423e576098ff10dc4ddab7
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-05-15 08:17:40 +0630

    Todo Day 7-2-4  : Adding comment

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ while True:
             todos = file.readlines()
             file.close()
 
+            # new_todo = [item.strip('\n') for item in todos]
+
             for index, item in enumerate(todos):
                 item = item.strip('\n')
                 row = f"{index + 1}-{item}"
