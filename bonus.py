88f09a77c6b025cb682f3d9cc9a19685204f847d
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-05-17 20:47:53 +0630

    Todo Day 8-5  : Bonus

--- bonus.py
+++ bonus.py
@@ -1,5 +1,7 @@
-filenames = ['1.dog', "2.cat", "3.fish"]
+date = input("Enter today's date: ")
+mood = input("How do you rate your mood today from 1 to 10? ")
+thoughts =  input("Let your thoughts flow: \n")
 
-filenames = [filename.replace(".", "-") + '.txt' for filename in filenames]
-
-print(filenames)
+with open(f"journal/{date}.txt", "w") as file:
+    file.write(mood + 2 * '\n')
+    file.write(thoughts)
