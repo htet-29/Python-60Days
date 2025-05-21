4d94f0ff386164885e66c93821067c3259bb1c14
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-05-21 21:11:59 +0630

    Todo Day 10-6 : Exercise 4

--- exercise.py
+++ exercise.py
@@ -1,6 +1,8 @@
-colors = [11, 34, 98, 43, 45, 54, 54]
+waiting_list = ["john", "marry"]
+name = input("Enter name: ")
 
-for i in colors:
-    if i > 50:
-        print(i)
-
+try:
+    number = waiting_list.index(name)
+    print(f"{name}'s turn is {number}.")
+except ValueError:
+    print(f"{name} is not in the list.")
