f3d128382fe99e761874e690f8be6fbd0a1d6348
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-05-21 21:09:28 +0630

    Todo Day 10-6 : Exercise 3

--- exercise.py
+++ exercise.py
@@ -1,11 +1,6 @@
-try:
-    total = float(input("Enter total value: "))
-    value = float(input("Enter value: "))
-    percent = (value / total) * 100
-    print(f"This is {percent}%")
-except ValueError:
-    print("You need to enter a number. Run the program again.")
-except ZeroDivisionError:
-    print("Your total value can not be zero.")
+colors = [11, 34, 98, 43, 45, 54, 54]
 
+for i in colors:
+    if i > 50:
+        print(i)
 
