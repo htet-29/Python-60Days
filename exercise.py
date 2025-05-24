c60a8a8a40aba53f72b5a64266d2d47ef06419b8
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-05-24 10:54:24 +0630

    Todo Day 12 - 6 : Exercise 3

--- exercise.py
+++ exercise.py
@@ -1,33 +1,5 @@
-password = input("Enter your password: ")
+def average(_list):
+    return sum(_list) / len(_list)
 
 
-def password_strength(_password):
-    result = {}
-
-    length = len(_password)
-
-    if length >= 8:
-        result['length'] = True
-    else:
-        result['length'] = False
-
-
-    upper = False
-    digit = False
-
-    for char in _password:
-        if char.isupper():
-            upper = True
-        if char.isdigit():
-            digit = True
-
-    result['upper'] = upper
-    result['digit'] = digit
-
-    if all(result.values()):
-        return "Strong password"
-    else:
-        return "Weak password"
-
-
-print(password_strength(password))
+print(average([1,2,3,4,5]))
