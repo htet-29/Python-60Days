5b553e424d52089b895dbe4a841392aee9456f13
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-05-24 11:43:16 +0630

    Todo Day 13 - 3 : Documenting the code with docs-string

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 def get_todos_from_file(filepath="todos.txt"):
+    """Read a text file and return a list of todo-item."""
     with open(filepath, 'r') as _file:
         _todos = _file.readlines()
     return _todos
 
 
 def write_todos_to_file(_todos, filepath="todos.txt"):
+    """Write a list of todo-item to a text file."""
     with open(filepath, 'w') as _file:
         _file.writelines(_todos)
 
+
+print(help(get_todos_from_file))
+print(help(write_todos_to_file))
 
 while True:
     user_action = input("Enter add, show, edit, complete or exit: ")
