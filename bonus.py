68a48bebaa3d8077398a73c383a84c2f72848115
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-05-24 12:23:33 +0630

    Todo Day 13 - 5 : Optimising the code with more decoupling

--- bonus.py
+++ bonus.py
@@ -1,18 +1,23 @@
 measure = input("Enter your measure: ")
 
-
-def convert_to_meter(_measure):
+def parsed(_measure):
     _measure = _measure.split(" ")
     feet = float(_measure[0])
     inches = float(_measure[1])
-    meter = feet * 0.3048 + inches * 0.0254
-    print(meter)
-    return meter
+    return {'feet': feet, 'inches': inches}
 
 
-result = convert_to_meter(measure)
+def convert_to_meter(feet, inches):
+    _meter = feet * 0.3048 + inches * 0.0254
+    return _meter
 
-if result < 1:
+
+units = parsed(measure)
+meter = convert_to_meter(units['feet'], units['inches'])
+
+print(f"{units['feet']} feet and {units['inches']} inches is equal to {meter}")
+
+if meter < 1:
     print("The kid is too small.")
 else:
     print("The kid can enter the ring.")
