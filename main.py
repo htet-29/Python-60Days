194e601e4c43af01f86702adbe8ab4378a0a44e9
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-05-27 20:05:02 +0630

    Todo Day 15 - 3 : Experiment 3 'csv' module

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import shutil
+import csv
 
-shutil.make_archive('ouput', 'zip', 'files')
+with open("weather.csv", 'r') as file:
+    data = list(csv.reader(file))
+
+
+city = input("Enter the city name: ")
+
+for row in data[1:]:
+    if row[0] == city:
+        print(row[1])
