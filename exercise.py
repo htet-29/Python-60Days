64f7fb2750caa2ddd52345e24f6f3441a1c94ad5
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-06-02 21:00:34 +0630

    Todo Day 17 - 6 : Exercise Unit Convertor

--- exercise.py
+++ exercise.py
@@ -1,31 +1,29 @@
 import FreeSimpleGUI as sg
-from unit_convertor import feet_inches_to_meters
 
-label_1 = sg.Text("Enter feet:")
-input_1 = sg.Input(key="feet")
 
-label_2 = sg.Text("Enter inches:")
-input_2 = sg.Input(key="inches")
+def km_to_miles(km):
+    return km / 1.6
+
+
+label = sg.Text("Kilometers: ")
+input_box = sg.InputText(tooltip="Enter todo", key="kms")
+miles_button = sg.Button("Convert")
 
 output = sg.Text(key="output")
 
-convert_btn = sg.Button("Convert")
-layout = [
-    [label_1, input_1],
-    [label_2, input_2],
-    [convert_btn, output],
-]
 
-window = sg.Window('Convertor', layout)
+window = sg.Window('Km to Miles Converter',
+                   layout=[[label, input_box], [miles_button, output]],
+                   font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
-    print(event, values)
-    feet = float(values["feet"])
-    inches = float(values["inches"])
-    result = str(feet_inches_to_meters(feet, inches)) + ' m'
-    window['output'].update(value=result)
-    if event == sg.WIN_CLOSED:
-        break
+    match event:
+        case "Convert":
+            km = float(values["kms"])
+            result = km_to_miles(km)
+            window['output'].update(value=result)
+        case sg.WIN_CLOSED:
+            break
 
 window.close()
--- unit_convertor.py
+++
@@ -1,8 +0,0 @@
-def feet_inches_to_meters(feet, inches):
-    meters = feet * 0.3048 + inches * 0.0254
-    return meters
-
-
-if __name__ == "__main__":
-    meter = feet_inches_to_meters(3, 4)
-    print(meter)
