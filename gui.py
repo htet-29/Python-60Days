c9ca88495ac50a0a6e7b196e1f2c9a4b6ae1f006
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-06-02 21:16:09 +0630

    Todo Day 18 -2 : Implemented Complete and Exit Button

--- gui.py
+++ gui.py
@@ -8,10 +8,14 @@ add_button = sg.Button('Add')
 todo_listbox = sg.Listbox(values=functions.get_todos_from_file(), key='todos_listbox',
                           enable_events=True, size=(45, 10))
 edit_button = sg.Button('Edit')
+complete_button = sg.Button('Complete')
+exit_button = sg.Button('Exit')
 
 window = sg.Window('My To-do App',
-                   layout=[[label], [input_text, add_button] ,
-                           [todo_listbox, edit_button]],
+                   layout=[[label],
+                           [input_text, add_button] ,
+                           [todo_listbox, edit_button, complete_button],
+                           [exit_button]],
                     font = ('Helvetica', 20))
 
 while True:
@@ -25,6 +29,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos_to_file(todos)
             window['todos_listbox'].update(values=todos)
+            window['todo_input'].update(value='')
         case 'Edit':
             todos = functions.get_todos_from_file()
             todo_to_edit = values['todos_listbox'][0]
@@ -35,6 +40,15 @@ while True:
             window['todos_listbox'].update(values=todos) # update a listbox
         case 'todos_listbox':
             window['todo_input'].update(value=values['todos_listbox'][0].strip())
+        case 'Complete':
+            todo_to_complete = values['todos_listbox'][0]
+            todos = functions.get_todos_from_file()
+            todos.remove(todo_to_complete)
+            functions.write_todos_to_file(todos)
+            window['todos_listbox'].update(values=todos)
+            window['todo_input'].update(value='')
+        case 'Exit':
+            break
         case sg.WIN_CLOSED:
             break
 
